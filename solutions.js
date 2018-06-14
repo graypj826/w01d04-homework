e8183c16f25669d6477043d41c60e17661103845
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-14 17:31:22 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,8 @@
+console.log("let's do this!")
+
+//Verbal questions
+
+// 2. // the difference between a parameter and an argument, is that the parameter is the input when the function is defined (const solutions = (parameter) =>) and the argument is the input when the function i scalled (solutions(argument))
+
+// 3. console.log has no effect on the function it prints the information to the console for the dev to know what is going on. Return ends the function and produces an output (if there is one)
+
