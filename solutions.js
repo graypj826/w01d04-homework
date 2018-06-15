1fee1ca3134dee98e6009fc09f7a4be46e836539
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-14 22:03:05 -0600

    Commit 2 - Palindrome.

--- solutions.js
+++ solutions.js
@@ -6,3 +6,28 @@ console.log("let's do this!")
 
 // 3. console.log has no effect on the function it prints the information to the console for the dev to know what is going on. Return ends the function and produces an output (if there is one)
 
+//Palindrome
+
+// Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
+
+const checkPalindrome = (string1) =>{
+	let stringToCompare = []
+	let string1Array = []
+	for (let i = string1.length-1; i >= 0; i--){
+		stringToCompare.push(string1[i].toUpperCase())
+	} 
+	console.log(stringToCompare)
+	for (let i = 0; i < string1.length; i++){
+		string1Array.push(string1[i].toUpperCase())
+	}
+	console.log(string1Array);
+	for (let i = 0; i < string1.length; i++){
+	if (string1Array[i] !== stringToCompare [i]){ 
+		return "False";
+		} else {
+		return "True"
+		}
+	}
+}
+
+console.log(checkPalindrome("Radar"));
